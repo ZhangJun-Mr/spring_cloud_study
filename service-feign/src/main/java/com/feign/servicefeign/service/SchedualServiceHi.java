52b3334df266aceea5c341c484239c4aa56489dc
zhangjun <zhangjun@qiatuchina.com>
2018-09-06 10:12:00 +0800

    init

---
+++ service-feign/src/main/java/com/feign/servicefeign/service/SchedualServiceHi.java
@@ -0,0 +1,13 @@
+package com.feign.servicefeign.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("SERVICE-HI")
+public interface SchedualServiceHi {
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
