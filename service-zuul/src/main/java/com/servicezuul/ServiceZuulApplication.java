ae22a79aa4c7755eab8cd9ffccebd6dfafd7265e
zhangjun <zhangjun@qiatuchina.com>
2018-09-07 09:46:17 +0800

    init

--- service-zuul/src/main/java/com/servicezuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/com/servicezuul/ServiceZuulApplication.java
@@ -1,9 +1,15 @@
-package com.servicezuul.servicezuul;
+package com.servicezuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ServiceZuulApplication {
 
     public static void main(String[] args) {
--- service-zuul/src/test/java/com/servicezuul/ServiceZuulApplicationTests.java
+++ service-zuul/src/test/java/com/servicezuul/ServiceZuulApplicationTests.java
@@ -1,4 +1,4 @@
-package com.servicezuul.servicezuul;
+package com.servicezuul;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
